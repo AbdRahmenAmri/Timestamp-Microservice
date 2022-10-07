f1127363d4d663977c258597561f39227eef1db4
AbdRahmen Amri <abdrahmen.3amri@gmail.com>
2022-10-07 21:08:00 +0100

    serving public folder && create "/api" controller

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+
+app.use('/static', express.static(path.join(__dirname, 'public')));
 
 app.get("/", (req,res) => {
     res.sendFile(`${__dirname}/views/index.html`);
+})
+
+app.get("/api", (req,res) => {
+    let date_now = Date.now();
+    let unix = new Number(date_now);
+    let d = new Date()
+    let utc = new Date(unix).toUTCString();
+
+    res.json({
+        unix:unix,
+        utc:utc
+    })
 })
 
 
